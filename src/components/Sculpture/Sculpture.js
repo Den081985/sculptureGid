3d9c800f9b8fe254a1177c714dc417181d07aa9b
Den081985 <pishnenkod@gmail.com>
2021-12-27 15:21:16 +0300

    add new methods

--- src/components/Sculpture/Sculpture.js
+++ src/components/Sculpture/Sculpture.js
@@ -1,23 +1,36 @@
-import { URL } from "../../../constants/url";
+import {
+  URL,
+  URI,
+  URI1,
+  URI2,
+  URI3,
+  URI4,
+  URI5,
+  URI6,
+  URI7,
+  URI8,
+  URI9,
+  URI10,
+  URI11,
+  URI12,
+} from "../../../constants/url";
 
 import { ROOT_INDEX } from "../../../constants/root";
 
 import { getDataApi } from "../../../utils/getDataApi";
 
+import Error from "../Error/Error";
+
 import * as classes from "./Sculpture.css";
 
 class Sculpture {
-  async render() {
-    const data = await getDataApi.getData(URL);
-
+  renderSculpture(data) {
     let htmlContent = ``;
-    data.forEach(({ id, name, img }) => {
-      console.log(id, name, img);
-
+    data.forEach(({ id, name, img, place, year, sculptureName }) => {
       htmlContent += `
-        <li class = "${classes.sculpture__item}">
+        <li class = "sculpture__li ${classes.sculpture__item}" data-id = "${id}")">
             <span class = "${classes.sculpture__name}">${name}</span>
-            <img class = "${classes.sculpture__img}" src = "${img}"/>
+            <img class = "img-contain ${classes.sculpture__img}" src = "${img}"/>
         
         </li>
       
@@ -29,6 +42,19 @@ class Sculpture {
     `;
     ROOT_INDEX.innerHTML = htmlWrapper;
   }
-}
+  async render() {
+    const data = await getDataApi.getData(URL);
 
+    data ? this.renderSculpture(data) : Error.render();
+  }
+
+  eventListener() {
+    document.querySelectorAll(".sculpture__li").forEach((element) => {
+      const id = element.getAttribute("data-id");
+      element.addEventListener("click", () => {
+        console.log(id);
+      });
+    });
+  }
+}
 export default new Sculpture();
