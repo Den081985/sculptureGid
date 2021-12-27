557094c9cb79a846b7fb795b6f23b92ee1de9fd5
Den081985 <pishnenkod@gmail.com>
2021-12-27 15:16:47 +0300

    add new constants

--- constants/url.js
+++ constants/url.js
@@ -1,1 +1,27 @@
 export const URL = "http://myjson.dit.upm.es/api/bins/dlll";
+
+const URI = "http://myjson.dit.upm.es/api/bins/";
+
+const URI1 = "4map";
+
+const URI2 = "4u0h";
+
+const URI3 = "34a9";
+
+const URI4 = "ic4h";
+
+const URI5 = "7u1d";
+
+const URI6 = "5wld";
+
+const URI7 = "aemp";
+
+const URI8 = "cri9";
+
+const URI9 = "4fcp";
+
+const URI10 = "4n2h";
+
+const URI11 = "2pmh";
+
+const URI12 = "9ctl";
