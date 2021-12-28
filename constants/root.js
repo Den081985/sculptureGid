2264270b9c55664249c4d3cf13e09ac5913ce7e4
Den081985 <pishnenkod@gmail.com>
2021-12-28 13:39:03 +0300

    add header const

--- constants/root.js
+++ constants/root.js
@@ -1,3 +1,5 @@
 export const ROOT_INDEX = document.getElementById("root");
 
 export const ROOT_MODAL = document.getElementById("modal");
+
+export const ROOT_HEADER = document.getElementById("header");
