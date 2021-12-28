66a1fe9dfbdeb80e08ffc351a009e57d492edc4f
Den081985 <pishnenkod@gmail.com>
2021-12-28 13:47:34 +0300

    add eventListener

--- src/components/Sculpture/Sculpture.js
+++ src/components/Sculpture/Sculpture.js
@@ -1,23 +1,10 @@
-import {
-  URL,
-  URI,
-  URI1,
-  URI2,
-  URI3,
-  URI4,
-  URI5,
-  URI6,
-  URI7,
-  URI8,
-  URI9,
-  URI10,
-  URI11,
-  URI12,
-} from "../../../constants/url";
+import { URL } from "../../../constants/url";
 
 import { ROOT_INDEX } from "../../../constants/root";
 
 import { getDataApi } from "../../../utils/getDataApi";
+
+import Description from "../Description/Description";
 
 import Error from "../Error/Error";
 
@@ -26,7 +13,7 @@ import * as classes from "./Sculpture.css";
 class Sculpture {
   renderSculpture(data) {
     let htmlContent = ``;
-    data.forEach(({ id, name, img, place, year, sculptureName }) => {
+    data.forEach(({ id, name, img }) => {
       htmlContent += `
         <li class = "sculpture__li ${classes.sculpture__item}" data-id = "${id}")">
             <span class = "${classes.sculpture__name}">${name}</span>
@@ -51,8 +38,68 @@ class Sculpture {
   eventListener() {
     document.querySelectorAll(".sculpture__li").forEach((element) => {
       const id = element.getAttribute("data-id");
-      element.addEventListener("click", () => {
-        console.log(id);
+      element.addEventListener("click", async () => {
+        if (id === "el1") {
+          const { place, year, sculptureName, img } = await getDataApi.getData(
+            "http://myjson.dit.upm.es/api/bins/8bs9"
+          );
+          Description.render(place, year, sculptureName, img);
+        } else if (id === "el2") {
+          const { place, year, sculptureName, img } = await getDataApi.getData(
+            "http://myjson.dit.upm.es/api/bins/da0x"
+          );
+          Description.render(place, year, sculptureName, img);
+        } else if (id === "el3") {
+          const { place, year, sculptureName, img } = await getDataApi.getData(
+            "http://myjson.dit.upm.es/api/bins/bs0h"
+          );
+          Description.render(place, year, sculptureName, img);
+        } else if (id === "el4") {
+          const { place, year, sculptureName, img } = await getDataApi.getData(
+            "http://myjson.dit.upm.es/api/bins/bzq9"
+          );
+          Description.render(place, year, sculptureName, img);
+        } else if (id === "el5") {
+          const { place, year, sculptureName, img } = await getDataApi.getData(
+            "http://myjson.dit.upm.es/api/bins/fg19"
+          );
+          Description.render(place, year, sculptureName, img);
+        } else if (id === "el6") {
+          const { place, year, sculptureName, img } = await getDataApi.getData(
+            "http://myjson.dit.upm.es/api/bins/989p"
+          );
+          Description.render(place, year, sculptureName, img);
+        } else if (id === "el7") {
+          const { place, year, sculptureName, img } = await getDataApi.getData(
+            "http://myjson.dit.upm.es/api/bins/7atp"
+          );
+          Description.render(place, year, sculptureName, img);
+        } else if (id === "el8") {
+          const { place, year, sculptureName, img } = await getDataApi.getData(
+            "http://myjson.dit.upm.es/api/bins/ig25"
+          );
+          Description.render(place, year, sculptureName, img);
+        } else if (id === "el9") {
+          const { place, year, sculptureName, img } = await getDataApi.getData(
+            "http://myjson.dit.upm.es/api/bins/3nnh"
+          );
+          Description.render(place, year, sculptureName, img);
+        } else if (id === "el10") {
+          const { place, year, sculptureName, img } = await getDataApi.getData(
+            "http://myjson.dit.upm.es/api/bins/cnq5"
+          );
+          Description.render(place, year, sculptureName, img);
+        } else if (id === "el11") {
+          const { place, year, sculptureName, img } = await getDataApi.getData(
+            "http://myjson.dit.upm.es/api/bins/8lw5"
+          );
+          Description.render(place, year, sculptureName, img);
+        } else if (id === "el12") {
+          const { place, year, sculptureName, img } = await getDataApi.getData(
+            "http://myjson.dit.upm.es/api/bins/d3xh"
+          );
+          Description.render(place, year, sculptureName, img);
+        }
       });
     });
   }
