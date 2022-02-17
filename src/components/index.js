010951745b646ab4eebcb9285c9074b4c875b219
Den081985 <pishnenkod@gmail.com>
2022-02-18 00:03:00 +0300

    добавлен Auth.authListener

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import App from "./App/App";
 
 import Sculpture from "./Sculpture/Sculpture";
+import Auth from "./Auth/Auth";
 
 (async () => {
   await App.render();
   Sculpture.eventListener();
+  Auth.authListener();
 })();
