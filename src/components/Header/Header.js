8770aee63c8e61ccc55edca7bdbaeb8f3d65ec5b
Den081985 <pishnenkod@gmail.com>
2022-02-18 12:07:58 +0300

    изменения в html-шаблоне

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,8 +9,8 @@ class Header {
             <span class = "${classes.header__span}">Городская скульптура Таганрога</span>
             <div class="${classes.header__items}">
                   <span id="auth" class=" ${classes.header__item}" data-id="auth">Регистрация</span>
-                  <span class="${classes.header__item}" data-id="enter">Войти</span>
-                  <span class="${classes.header__item}" data-id="comment">Комментарии</span>
+                  <span id="enter"class="${classes.header__item}" data-id="enter">Войти</span>
+                  <span id="comment"class="${classes.header__item}" data-id="comment">Комментарии</span>
                 </div>
         </div>
       
